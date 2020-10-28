f5994e331b65793fa8104638cd0759899af82b7c
dannynurdin <dannyfachrul@gmail.com>
2020-10-28 18:28:48 +0700

    add forms and template

--- testdb/forms.py
+++ testdb/forms.py
@@ -10,3 +10,19 @@ class StudentsForm(forms.ModelForm):
     class Meta:
         model = Students
         fields = '__all__'
+
+class ClassesForm(forms.ModelForm):
+    class Meta:
+        model = Classes
+        fields = '__all__'
+
+class AbsentsForm(forms.ModelForm):
+    class Meta:
+        model = Absents
+        fields = '__all__'
+
+class RecordsForm(forms.ModelForm):
+    class Meta:
+        model = Records
+        fields = '__all__'
+
